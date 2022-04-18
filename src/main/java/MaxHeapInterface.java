83642f1152009d00ac57002655e30ca7f46b3161
amehr2003 <mehrazarin.a@gmail.com>
2022-04-18 12:24:30 -0700

    Added methods to MaxHeapInterface and added private fields to MaxHeap class.

--- src/main/java/MaxHeapInterface.java
+++ src/main/java/MaxHeapInterface.java
@@ -1,4 +1,26 @@
-public interface MaxHeapInterface {
+public interface MaxHeapInterface <T extends Comparable<? super T>> {
+    /**
+     * clears contents of the MaxHeap
+     */
     public void clear();
+
+    /**
+     * returns int containing the size of the MaxHeap
+     */
     public int getSize();
+
+    /**
+     * adds a new object entry to the MaxHeap
+     */
+    public void add(T newEntry);
+
+    /**
+     *  returns the max object of the MaxHeap
+     */
+    public T getMax();
+
+    /**
+     * removes the max entry from the MaxHeap and returns the object
+     */
+    public T removeMax();
 }
