f1ef75a676aa0c98215c7da3fe001a0dced5f8f7
shree-anya <98497622+shree-anya@users.noreply.github.com>
2022-04-18 22:28:11 -0700

    added methods to MaxHeap

--- src/main/java/MaxHeap.java
+++ src/main/java/MaxHeap.java
@@ -35,13 +35,46 @@ public final class MaxHeap<T extends Comparable <? super T>>
 
     }
 
-    // method for adding to heap using optimal method
+    private void reheap(int rootIndex)
+    {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+        while (!done && (leftChildIndex <= lastIndex) )
+        {
+            int largerChildIndex = leftChildIndex; // Assume larger
+            int rightChildIndex = leftChildIndex + 1;
+            if ( (rightChildIndex <= lastIndex) &&
+                    heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+            {
+                largerChildIndex = rightChildIndex;
+            } // end if
+            if (orphan.compareTo(heap[largerChildIndex]) < 0)
+            {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+            }
+            else
+                done = true;
+        } // end while
+        heap[rootIndex] = orphan;
+    } // end reheap
 
     //remove method for MaxHeap
     public T removeMax()
     {
-        return ;
-    }
+        checkInitilization(); // Ensure initialization of data fields
+        T root = null;
+        if (!isEmpty())
+        {
+            root = heap[1]; // Return value
+            heap[1] = heap[lastIndex]; // Form a semiheap
+            lastIndex--; // Decrease size
+            reheap(1); // Transform to a heap
+        } // end if
+        return root;
+    } // end removeMax
 
 
 }
