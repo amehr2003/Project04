4391b045316f152a1174df2a1e8da0ecbe989dfc
amehr2003 <mehrazarin.a@gmail.com>
2022-04-18 20:15:30 -0700

    added skeleton code.

--- src/main/java/OutputFile.java
+++ src/main/java/OutputFile.java
@@ -1,3 +1,17 @@
+/**
+ * o Create a max-heap using the sequential insertions, for those 100 integers.
+ * o Output the first 10 integers of your array, into the output file
+ * o Output the number of swaps performed, into the output file
+ * o Perform 10 removals on the heap
+ * o Output the first 10 integers of the resulting array, into the output file
+ *
+ * o Create a max-heap using the optimal method, for those 100 integers
+ * o Output the first 10 integers of your array, into the output file
+ * o Output the number of swaps performed, into the output file
+ * o Perform 10 removals on the heap
+ * o Output the first 10 integers in the resulting array, into the output file
+ */
+
 public class OutputFile {
     public static void main(String[] args)
     {
